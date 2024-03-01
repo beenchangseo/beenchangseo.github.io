920d2607a829d5ab4ba0a35394fa5dfeeabcf719
beenchangseo <beenchangseo@gmail.com>
2024-03-01 21:26:09 +0900

    dark mode 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import './globals.css';
+import Providers from '../components/mode/Provider';
 import Layout from '@/components/Layout';
 
 const inter = Inter({subsets: ['latin']});
@@ -16,9 +17,11 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                <Layout>{children}</Layout>
+        <html lang="en" className={`transition duration-300 ${inter.className}`}>
+            <body /* className={inter.className} */>
+                <Providers>
+                    <Layout>{children}</Layout>
+                </Providers>
             </body>
         </html>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,13 @@
 'use client';
 import {useEffect, useRef, useState} from 'react';
 import Nav from './Nav';
-import Image from 'next/image';
-import Head from 'next/head';
 import Link from 'next/link';
-
-type Theme = null | 'dark' | 'light';
+import DarkModeButton from './mode/DarkMode';
 
 export default function Header() {
     const headerRef = useRef<HTMLElement>(null);
     const toggleRef = useRef<HTMLDivElement>(null);
     const [onToggle, setOnToggle] = useState<boolean>(false);
-    const [theme, setTheme] = useState<Theme>(null);
-
-    const handleTheme = () => {
-        const newTheme = theme === 'dark' ? 'light' : 'dark';
-        setTheme(newTheme);
-        window.localStorage.setItem('theme', newTheme);
-        document.body.className = newTheme;
-    };
 
     const handleScroll = () => {
         if (window.scrollY > 0) {
@@ -36,7 +25,6 @@ export default function Header() {
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
-        setTheme(document.body.className as Theme);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
@@ -44,65 +32,18 @@ export default function Header() {
 
     return (
         <>
-            <Head>
-                <title>Maintain Hoon</title>
-            </Head>
             <header
                 ref={headerRef}
                 className="sticky top-0 left-0 w-full z-10 h-20 font-mono transition duration-500 bg-white dark:bg-[#111111]"
             >
-                <div className="text-black max-w-screen-md h-20 flex flex-nowrap items-center justify-between m-auto px-8">
+                <div className="max-w-screen-md h-20 flex flex-nowrap items-center justify-between m-auto px-8">
                     <Link href="/">
-                        {theme === 'dark' ? (
-                            <h1 className="text-white">BEENCHANGSEO</h1>
-                        ) : (
-                            <h1 className="text-balck">BEENCHANGSEO</h1>
-                        )}
-                        {/* TODO: 로고 만들어서 코드 수정하기 */}
-                        {/* {theme === 'dark' ? (
-                            <Image
-                                src="/images/logo_dark_mode.png"
-                                alt="profile"
-                                width={180}
-                                height={30}
-                            />
-                        ) : (
-                            <Image
-                                src="/images/logo_light_mode.png"
-                                alt="profile"
-                                width={180}
-                                height={30}
-                            />
-                        )} */}
+                        <h1>BEENCHANGSEO.DEV</h1>
                     </Link>
+
                     <div className="flex flex-nowrap gap-8 items-center">
-                        {/* Main logo 영역 */}
-                        <button type="button" className="m-0 p-0" onClick={handleTheme}>
-                            {theme === 'dark' ? (
-                                <Image src="/svg/moon.svg" alt="dark mode" width={30} height={30} />
-                            ) : (
-                                <Image src="/svg/sun.svg" alt="light mode" width={30} height={30} />
-                            )}
-                        </button>
+                        <DarkModeButton />
 
-                        {/* 토글 영역 */}
-                        <button type="button" className="m-0 p-0 sm:hidden" onClick={handleToggle}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-7 h-7 transition duration-500 stroke-black dark:stroke-white"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                                />
-                            </svg>
-                        </button>
-
-                        {/* 메뉴 영역 */}
                         <div className="flex-nowrap items-center justify-center gap-5 text-center hidden sm:flex">
                             <Nav type="normal" />
                         </div>
