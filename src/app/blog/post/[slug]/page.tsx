373f6e6f244bc157c20b018b6b2932b5522c4390
beenchangseo <beenchangseo@gmail.com>
2024-09-12 16:55:43 +0900

    방문자수 표시 기능 추가

--- src/app/blog/post/[slug]/page.tsx
+++ src/app/blog/post/[slug]/page.tsx
@@ -3,6 +3,7 @@ import {Mdx} from '../../../../components/Mdx';
 import {allPosts} from 'contentlayer/generated';
 import {Metadata} from 'next';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export async function generateStaticParams() {
     return allPosts.map((post) => ({
@@ -54,14 +55,14 @@ const Post = ({params}: {params: {slug: string}}) => {
     };
 
     const categoryButtonStyle =
-        'h-8 px-3 m-1 text-xs transition-colors duration-150 bg-gray-200 rounded-lg cursor-pointer focus:shadow-outline hover:bg-gray-300';
+        'h-8 px-3 m-1 text-xs border-2 border-gray-700 dark:border-gray-300 transition-colors duration-150 rounded-lg cursor-pointer focus:shadow-outline hover:bg-gray-300';
 
     return (
         <>
             <section>
                 <div className="mt-10 pb-10 border-b-2 mb-10 prose dark:prose-invert">
                     <h1 className="mb-8 font-bold text-2xl sm:text-4xl font-mono">{post.title}</h1>
-                    <div className="flex-auto mb-16">
+                    <div className="flex-auto mb-4">
                         {post.category.map((item, index) => (
                             <button className={categoryButtonStyle} key={index}>
                                 <Link href={{pathname: '/category', query: {category: item}}}>
@@ -70,13 +71,24 @@ const Post = ({params}: {params: {slug: string}}) => {
                             </button>
                         ))}
                     </div>
-                    <div className="flex-auto mb-16">
+                    <div className="flex items-center justify-between mb-16">
                         <span className="text-sm">
                             <a href="https://beenchangseo.github.io/">beenchangseo</a>
                         </span>
                         <span className="ml-1 mr-1">·</span>
                         <span className="text-sm font-medium text-gray-500">
                             {formatDate(post.date)}
+                        </span>
+                        <span className="flex items-center ml-auto">
+                            <a href="https://hits.seeyoufarm.com">
+                                <Image
+                                    className="mb-0 mt-0"
+                                    src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fbeenchangseo.github.io&count_bg=%234B4B4B&title_bg=%23929292&icon=tencentqq.svg&icon_color=%23E7E7E7&title=TODAY&edge_flat=false"
+                                    alt="Landscape picture"
+                                    width={130}
+                                    height={20}
+                                />
+                            </a>
                         </span>
                     </div>
                     <Mdx code={post.body.code} />
