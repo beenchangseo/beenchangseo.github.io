df3960380b5dd515de9025da4ae2d7e9651e0002
changbeen.seo <changbeen.seo@dnsevercorp.com>
2024-10-28 11:17:27 +0900

    Add career nav link

--- src/data/navlinks.ts
+++ src/data/navlinks.ts
@@ -2,4 +2,5 @@ export const nav: {title: string; location: string}[] = [
     {title: 'Home', location: '/'},
     {title: 'Blog', location: '/blog'},
     {title: 'Category', location: '/category'},
+    {title: 'Career', location: '/career'},
 ];
